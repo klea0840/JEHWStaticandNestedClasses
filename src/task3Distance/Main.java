5f18ec31d51214887300f06798b58d4031d0fe99
papayaspark <papayaspark@protonmail.com>
2020-09-23 15:30:40 +0300

    Замечания по домашке

--- src/task3Distance/Main.java
+++ src/task3Distance/Main.java
@@ -6,9 +6,18 @@ public class Main {
 
         double i = 5;
 
-        Distance.Converter convert = new Distance.Converter(i);
-        Distance.print();
-        convert.convertToMeters(i);
-        convert.convertToMiles(i);
+//        Distance.Converter convert = new Distance.Converter(i);
+//        Distance.print();
+//        convert.convertToMeters(i);
+//        convert.convertToMiles(i);
+
+        // Использование методов вложенного класса напрямую
+        System.out.println("In meters: " + Distance.Converter.convertToMeters(i));
+        System.out.println("In miles: " + Distance.Converter.convertToMiles(i));
+
+        // ...через метод внешнего класса
+        Distance d = new Distance(45);
+        System.out.println("In miles: " + d.toMiles());
+
     }
 }
